3f9b914796d5d02732d69cf0d1e94838ec6b605c
Gesang Paudra J <g.paudra@gmail.com>
2023-09-08 11:47:35 +0700

    update arduino posting script

--- arduino_posting.php
+++ arduino_posting.php
@@ -1,5 +1,8 @@
 <?php
-// Connect to your database (you may need to modify the connection details)
+// Set zona waktu
+date_default_timezone_set('Asia/Jakarta');
+
+// Properti koneksi database
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,25 +10,25 @@ $dbname = "kopi";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+// Cek koneksi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get current date and time
-$tanggaljam = date('Y-m-d H:i:s');
+// set format tanggal waktu
+$current_time = date('Y-m-d H:i:s');
 
 if (!empty($_POST['suhu']) && !empty($_POST['kelembaban']) && !empty($_POST['pH'])) {
     $suhu = $_POST['suhu'];
     $kelembaban = $_POST['kelembaban'];
     $pH = $_POST['pH'];
 
-    // Prepare and execute the SQL query
+    // Menyiapkan query
     $sql = "INSERT INTO sensor_data (tanggaljam, suhu, kelembaban, pH) VALUES (?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
 
     // Use appropriate placeholders: "s" for string, "d" for double, "i" for integer
-    $stmt->bind_param("ssdd", $tanggaljam, $suhu, $kelembaban, $pH);
+    $stmt->bind_param("ssdd", $current_time, $suhu, $kelembaban, $pH);
 
     if ($stmt->execute()) {
         echo "OK";
